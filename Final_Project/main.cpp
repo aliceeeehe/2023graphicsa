b7b6f93667b996d65cb684aec3276b099a6d405a
aliceeeehe <alice9202022@gmail.com>
2023-05-17 11:23:05 +0800

    week14_挑選關節

--- Final_Project/main.cpp
+++ Final_Project/main.cpp
@@ -11,18 +11,19 @@ GLMmodel*rightarm=NULL;
 GLMmodel*rightuparm=NULL;
 GLMmodel*rightleg=NULL;
 GLMmodel*rightupleg=NULL;
-int show[10] = {0,1,2,3,4,5,6,7,8,9};
+int show[10] = {1,1,1,1,1,1,1,1,1,1};
+int ID=0;
 void keyboard(unsigned char key,int x,int y){
-    if(key=='0') show[0] = !show[0];
-    if(key=='1') show[1] = !show[1];
-    if(key=='2') show[2] = !show[2];
-    if(key=='3') show[3] = !show[3];
-    if(key=='4') show[4] = !show[4];
-    if(key=='5') show[5] = !show[5];
-    if(key=='6') show[6] = !show[6];
-    if(key=='7') show[7] = !show[7];
-    if(key=='8') show[8] = !show[8];
-    if(key=='9') show[9] = !show[9];
+    if(key=='0') ID=0;
+    if(key=='1') ID=1;
+    if(key=='2') ID=2;
+    if(key=='3') ID=3;
+    if(key=='4') ID=4;
+    if(key=='5') ID=5;
+    if(key=='6') ID=6;
+    if(key=='7') ID=7;
+    if(key=='8') ID=8;
+    if(key=='9') ID=9;
     glutPostRedisplay();
 }
 FILE*fout=NULL;
@@ -46,22 +47,49 @@ void display()
             rightleg=glmReadOBJ("model/rightleg.obj");
             rightupleg=glmReadOBJ("model/rightupleg.obj");
         }
-        if(show[0]) glmDraw(body, GLM_MATERIAL);
+        if(ID==0) glColor3f(1,0,0);
+        else glColor3f(1,1,1);
+        if(show[0]) glmDraw(body,GLM_MATERIAL);
+
+        if(ID==1) glColor3f(1,0,0);
+        else glColor3f(1,1,1);
         if(show[1]) glmDraw(head, GLM_MATERIAL);
+
+        if(ID==2) glColor3f(1,0,0);
+        else glColor3f(1,1,1);
         if(show[2]) glmDraw(leftarm, GLM_MATERIAL);
+
+        if(ID==3) glColor3f(1,0,0);
+        else glColor3f(1,1,1);
         if(show[3]) glmDraw(leftuparm, GLM_MATERIAL);
+
+        if(ID==4) glColor3f(1,0,0);
+        else glColor3f(1,1,1);
         if(show[4]) glmDraw(leftleg, GLM_MATERIAL);
+
+        if(ID==5) glColor3f(1,0,0);
+        else glColor3f(1,1,1);
         if(show[5]) glmDraw(leftupleg, GLM_MATERIAL);
+
+        if(ID==6) glColor3f(1,0,0);
+        else glColor3f(1,1,1);
         if(show[6]) glmDraw(rightarm, GLM_MATERIAL);
+
+        if(ID==7) glColor3f(1,0,0);
+        else glColor3f(1,1,1);
         if(show[7]) glmDraw(rightuparm, GLM_MATERIAL);
+
+        if(ID==8) glColor3f(1,0,0);
+        else glColor3f(1,1,1);
         if(show[8]) glmDraw(rightleg, GLM_MATERIAL);
+
+        if(ID==9) glColor3f(1,0,0);
+        else glColor3f(1,1,1);
         if(show[9]) glmDraw(rightupleg, GLM_MATERIAL);
         glPushMatrix();
             glTranslatef(teapotX,teapotY,0);
-
-                if(show[2]) glmDraw(leftarm,GLM_MATERIAL);
             glPopMatrix();
-            if(show[3]) glmDraw(leftuparm,GLM_MATERIAL);
+
         glPopMatrix();
         glutSwapBuffers();
 }
